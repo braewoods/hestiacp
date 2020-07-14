2812492a141715d34bbad129b84ba4c5804bfb0f
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2020-07-14 13:49:27 +0200

    repalced worng ; with of ,

--- web/inc/i18n/en.php
+++ web/inc/i18n/en.php
@@ -877,6 +877,6 @@ $LANG['en'] = array(
     //list DNS
     'Records_DNS' => 'Records',
     //Dowload Backup
-    'BACKUP_DOWNLOAD_SCHEDULED' => 'Download for Backup has been scheduled.';
+    'BACKUP_DOWNLOAD_SCHEDULED' => 'Download for Backup has been scheduled.',
     
 );
